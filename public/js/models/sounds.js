3ccdf308065439f29d3961976fe21cc8df34643f
Samuel Toriel <samuel.toriel@rackspace.com>
2013-07-20 16:55:11 -0400

    Add sounds

---
+++ public/js/models/sounds.js
@@ -0,0 +1,26 @@
+define(
+  [
+    'backbone'
+  ],
+  function(Backbone) {
+    var Sound;
+
+    Sound = Backbone.Model.extend({
+      defaults: {
+        types: {
+          'destroyEntity': new Audio("/public/sounds/destroy_entity.wav"),
+          'buildEntity': new Audio("/public/sounds/build_entity.wav")
+        }
+      },
+      play: function (soundName) {
+        var sound;
+
+        sound = this.get('types')[soundName];
+        sound.currentTime = 0;
+        sound.play();
+      }
+    });
+
+    return Sound;
+  }
+);
