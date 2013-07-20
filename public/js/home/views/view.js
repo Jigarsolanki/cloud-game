71bc1f536a0b5a1b93c51bfdd2a6e5890be11c2a
Jigar Solanki <jigar234@gmail.com>
2013-07-20 11:22:09 -0400

    Add server on demand

--- public/js/home/views/view.js
+++ public/js/home/views/view.js
@@ -20,6 +20,7 @@ define(
       particleTimer: null,
       firstDestination: null,
       requestComponents: [],
+      entities: [],
       initialize: function() {
         this.render();
       },
@@ -32,16 +33,32 @@ define(
 
         this.requestContainer = new RequestContainer({ stage: this.stage }).get();
 
-        server1 = new Server({ x: 400, y: 200 });
-        serverComponent1 = new ServerComponent({model: server1, stage: this.stage});
+        // server1 = new Server({ x: 400, y: 200 });
+        // serverComponent1 = new ServerComponent({model: server1, stage: this.stage});
 
-        lb1 = new Loadbalancer({ x: 200, y: 200 });
-        lb1.addNode(server1);
-        lbComponent1 = new LoadbalancerComponent({ model: lb1, stage: this.stage });
+        // lb1 = new Loadbalancer({ x: 200, y: 200 });
+        // lb1.addNode(server1);
+        // lbComponent1 = new LoadbalancerComponent({ model: lb1, stage: this.stage });
 
-        this.firstDestination = lb1;
+        //this.firstDestination = lb1;
 
+        this.bindButtons();
         this.start();
+      },
+      bindButtons: function () {
+        $('#add_server').on('click', _.bind(function () {
+          this.addServer();
+        }, this));
+      },
+      addServer: function () {
+        var server, serverComponent;
+
+        server = new Server({ x: 600, y: this.canvas.height * Math.random() });
+        serverComponent = new ServerComponent({model: server, stage: this.stage});
+        if (this.entities.length === 0) {
+          this.firstDestination = server;
+          this.entities.push(server);
+        }
       },
       start: function () {
         this.particleTimer = setInterval(_.bind(function () {
